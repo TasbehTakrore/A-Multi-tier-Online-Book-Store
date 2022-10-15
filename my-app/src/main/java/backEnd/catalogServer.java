fccc53afeac137cd01f619c4f005013f2e9835b2
TasbehTakrore <tasbeh.takrore@gmail.com>
2022-10-15 21:17:00 +0300

    edit search query, and create info query, for catalog server.

--- my-app/src/main/java/backEnd/catalogServer.java
+++ my-app/src/main/java/backEnd/catalogServer.java
@@ -16,6 +16,7 @@ import java.util.List;
 public class catalogServer {
 	
 	static String searchResponse="";
+	static String iteamResponse="";
 	static String comma="";
 	static int f = 0; 
 	
@@ -26,7 +27,7 @@ public class catalogServer {
 				f=1;
 				comma += ",";}
 	}
-	
+
 	
 	 static String searchForTopic(String topic) throws IOException, CsvException {
 		 searchResponse ="[";
@@ -36,18 +37,46 @@ public class catalogServer {
 			   List<String[]> r = reader.readAll();
 			      r.forEach(x -> ifTopicFound(x,topic)); } 
 		  searchResponse += "\n]"; 
-		
+		 
+		  if(searchResponse.equals("[\n]")) {
+			  searchResponse = "\n  {\n     \"message\": \"This topic does not exist!\" \n  }";
+		  }
 		return searchResponse;
 	}
 
+	static void ifIteamFound(String[] x, String iteam){
+			if ( x[0].equals(iteam))
+				{iteamResponse = "\n  {\n     \"title\": \"" + x[1]+ "\",\n     \"quantity\":"+ x[3]+ ",\n     \"price\":"+ x[2]+"\n  }";
+					f = 1;
+				}}
+			 
+	 
+	 static String searchForIteam(String iteam) throws IOException, CsvException {
+		 iteamResponse="\n  {\n     \"message\": \"This item does not exist!"+"\"\n  }";
+		  try (CSVReader reader = new CSVReader(new FileReader("C:\\Users\\USER\\git\\A-Multi-tier-Online-Book-Store\\my-app\\src\\main\\java\\backEnd\\catalogDatabase.CSV"))) {
+			   List<String[]> r = reader.readAll();
+			   f = 0;
+			   for( String[] arry: r) {
+				   ifIteamFound(arry,iteam.toString());
+		    		  if(f==1) break;
+			   }}
+			   
+		return iteamResponse;
+	}
+
+	 
 
 	    public static void main(String[] args) throws IOException, CsvException {
 
-	        get("/search/:topic", (req,res)->{
+	        get("query/search/:topic", (req,res)->{
 	            res.type("application/json");
 	            return  searchForTopic(req.params(":topic").toLowerCase());
 	        });
 
+	        get("query/info/:iteam", (req,res)->{
+	            res.type("application/json");
+	            return  searchForIteam(req.params(":iteam").toLowerCase());
+	        });
 	    }
 	
 
